244e2eb7496fd575a2aba876dce8559293a6e8f2
Alma Maria Livingston <alma1@Almas-MBP.local.tld>
2015-06-02 13:08:33 -0700

    Cleaning up edit

--- client/front_controllers/quiz_front_controller.js
+++ client/front_controllers/quiz_front_controller.js
@@ -1,5 +1,5 @@
 //// Quiz Controller
-clickrModule.controller("quizController", function($scope, $routeParams, quizFactory) {
+clickrModule.controller("quizController", function($scope, $routeParams, $location, localStorageService, quizFactory) {
 	// call on factory to grab quiz with specific ID
 	quizFactory.getOneQuiz($routeParams.id, function(data){
 		$scope.quiz = data;
@@ -9,9 +9,9 @@ clickrModule.controller("quizController", function($scope, $routeParams, quizFac
 	$scope.updateQuiz = function(){
 		quizFactory.updateQuiz($routeParams.id, $scope.updatedQuiz, function(){
 			$scope.updatedQuiz = {};
-			// quizFactory.getOneQuiz($routeParams.id, function(data){
-			// 	$scope.quiz = data;
-			// });
-		})
+		});
+
+		// Redirect to dashboard
+		$location.path("/home/" + localStorageService.get("userid"));
 	}
 })
