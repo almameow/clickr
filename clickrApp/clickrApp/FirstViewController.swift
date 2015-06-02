ea9462c0bd08607d51e73fff5baa10d759c907dc
Courtney Jong <courtney.jong@gmail.com>
2015-06-02 13:10:53 -0700

    added sockets

---
+++ clickrApp/clickrApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clickrApp
+//
+//  Created by Courtney Jong on 5/29/15.
+//  Copyright (c) 2015 Courtney Jong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ clickrApp/clickrApp/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  clickrApp
+//
+//  Created by Courtney Jong on 5/29/15.
+//  Copyright (c) 2015 Courtney Jong. All rights reserved.
+//
+
+import Foundation
